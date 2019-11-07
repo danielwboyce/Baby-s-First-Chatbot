7aa82986c75d9edc213429cc8ac80f8f8dcb48e0
daniel <danielwboyce@gmail.com>
2019-11-06 17:11:25 -0700

    Reorganized the filesystem; made updates to main.py

--- chatbot-template/README.markdown
+++
@@ -1,4 +0,0 @@
-# Chatbot Template
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/danielwboyce/pen/abbYVOo](https://codepen.io/danielwboyce/pen/abbYVOo).
-
- 
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
+
+babysFirst = ChatBot(name='PyBot', read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
+
+corpus_trainer = ChatterBotCorpusTrainer(babysFirst)
+corpus_trainer.train('chatterbot.corpus.english')
+
+print(babysFirst.get_response("hi"))
+
+#this is a test comment for git
+
+
 
 app = Flask(__name__)
 
@@ -9,7 +23,7 @@ class Message(FlaskForm):
 	userMessage = StringField('message', validators[InputRequired()])
 	#chatbotResponse
 
-@app.route(' /', methods=['POST'])
+@app.route('s/', methods=['POST'])
 def home():
 	return render_template("home.html")
 
